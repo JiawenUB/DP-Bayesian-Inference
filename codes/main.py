957fa94979d988f28ba1595eebbf1757b363fa7b
update the pdf <jliu223@gmail.com>
2018-10-02 00:13:49 -0400

    update plots by concrete probabilities

--- codes/main.py
+++ codes/main.py
@@ -67,9 +67,7 @@ def draw_error_l1(errors, model, filename):
 	return
 
 
-
-
-def accuracy_study_discrete(sample_size,epsilon,delta,prior,observation):
+def row_discrete_probabilities(sample_size,epsilon,delta,prior,observation):
 	Bayesian_Model = BayesInferwithDirPrior(prior, sample_size, epsilon, delta)
 	Bayesian_Model._set_observation(observation)
 
@@ -89,8 +87,16 @@ def accuracy_study_discrete(sample_size,epsilon,delta,prior,observation):
 	candidates_classfied_by_steps = []
 
 	probabilities_exp_by_steps = []
-	probabilities_lap_by_steps = []
+	probabilities_lap_1_by_steps = []
+	probabilities_lap_2_by_steps = []
 
+	f_exp = open("data_" + str(observation) +"_exp.txt", "w")
+	f_lap_1 = open("data_" + str(observation) +"_lap_sensitivity1.txt", "w")
+	f_lap_2 = open("data_" + str(observation) +"_lap_sensitivity2.txt", "w")
+
+	f_exp.write("Candidates_of_the_same_steps, Hellinger Distance, Probabilities \n")
+	f_lap_1.write("Candidates_of_the_same_steps, Hellinger Distance, Probabilities \n")
+	f_lap_2.write("Candidates_of_the_same_steps, Hellinger Distance, Probabilities \n")
 	while i < len(sorted_scores):
 		j = i
 		candidates_for_print = []
@@ -104,34 +110,80 @@ def accuracy_study_discrete(sample_size,epsilon,delta,prior,observation):
 			i += 1
 		candidates_classfied_by_steps.append(candidates_for_classify)
 		probabilities_exp_by_steps.append(Bayesian_Model._SS_probabilities[j]*(i - j))
-		print "ExpMech: Pr[H(BI(x), r) = " + str(-sorted_scores[j][1]) + " ] = " + str(Bayesian_Model._SS_probabilities[j]*(i - j)) + " (r = " + str(candidates_for_print) +")"
-   
+		f_exp.write(str((candidates_for_print, -sorted_scores[j][1], Bayesian_Model._SS_probabilities[j]*(i - j))) + "\n")
+	f_exp.close()
+
 	# y = numpy.arange(0,4,1)
-	laplace_probabilities = {}
+	laplace_probabilities_1 = {}
+	laplace_probabilities_2 = {}
 	for i in range(len(Bayesian_Model._candidates)):
 		r = Bayesian_Model._candidates[i]
-		t = 1.0
+		t1 = 1.0
+		t2 = 1.0
 		# ylist = []
 		for j in range(len(r._alphas) - 1):
 			a = r._alphas[j] - Bayesian_Model._posterior._alphas[j]
-			t = t * 0.5 * (math.exp(- ((abs(a)) if (a >= 0) else (abs(a) - 1)) / (2.0/epsilon)) - math.exp(- ((abs(a) + 1) if (a >= 0) else (abs(a))) / (2.0/epsilon)))
+			t1 = t1 * 0.5 * (math.exp(- ((abs(a)) if (a >= 0) else (abs(a) - 1)) / (2.0/epsilon)) - math.exp(- ((abs(a) + 1) if (a >= 0) else (abs(a))) / (2.0/epsilon)))
+			t2 = t2 * 0.5 * (math.exp(- ((abs(a)) if (a >= 0) else (abs(a) - 1)) / (4.0/epsilon)) - math.exp(- ((abs(a) + 1) if (a >= 0) else (abs(a))) / (4.0/epsilon)))
+			
 		# 	ylist.append(a)
 		# yset = set(ylist)
-		laplace_probabilities[r] = t #/ (math.gamma(len(yset)) * (2 ** (len(list(filter(lambda a: a != 0, ylist))))))
+		laplace_probabilities_1[r] = t1 #/ (math.gamma(len(yset)) * (2 ** (len(list(filter(lambda a: a != 0, ylist))))))
+		laplace_probabilities_2[r] = t2 #/ (math.gamma(len(yset)) * (2 ** (len(list(filter(lambda a: a != 0, ylist))))))
 
 	for class_i in candidates_classfied_by_steps:
-		pro_i = 0.0
+		pro_i_1 = 0.0
+		pro_i_2 = 0.0
 		candidates_for_print = []
 		for c in class_i:
 			#print laplace_probabilities[c]
-			pro_i += laplace_probabilities[c]
+			pro_i_1 += laplace_probabilities_1[c]
+			pro_i_2 += laplace_probabilities_2[c]
 			candidates_for_print.append(c._alphas)
-		probabilities_lap_by_steps.append(pro_i)
-		
-		print "Laplace: Pr[H(BI(x), r) = " + str(-Bayesian_Model._candidate_scores[class_i[0]]) + " ] = " + str(pro_i) + " (r = " + str(candidates_for_print) +")"
-	print "ExpMech: Pr[H(BI(x), r) = 0.0 ] = " + str(probabilities_exp_by_steps[-1])
-	print "Laplace: Pr[H(BI(x), r) = 0.0 ] = " + str(probabilities_lap_by_steps[-1])
+		probabilities_lap_1_by_steps.append(pro_i_1)
+		probabilities_lap_2_by_steps.append(pro_i_2)
+		f_lap_1.write(str((candidates_for_print, -Bayesian_Model._candidate_scores[class_i[0]], pro_i_1)) + "\n")
+		f_lap_2.write(str((candidates_for_print, -Bayesian_Model._candidate_scores[class_i[0]], pro_i_2)) + "\n")
+	f_lap_1.close()
+	f_lap_2.close()	
 
+
+
+	plt.plot(steps[-100:], probabilities_exp_by_steps[-100:], 'ro', label=('Our ExpMech'))
+	# plt.plot(T, approximate_bounds, 'g^', label=('Expmech_SS zApproximate Bound'))
+	plt.plot(steps[-100:], probabilities_lap_1_by_steps[-100:], 'b^', label=('LapMech (sensitivity = 2)'))
+
+	plt.plot(steps[-100:], probabilities_lap_2_by_steps[-100:], 'gv', label=('LapMech (sensitivity = 4)'))
+
+	plt.xlabel("c / (steps from correct answer, in form of Hellinger Distance)")
+	plt.ylabel("Pr[H(BI(x),r) = c]")
+	plt.title("Discrete Probabilities")
+	plt.legend()
+	plt.grid()
+	plt.savefig("(data_"+ str(observation) + "_prior_"+ str(Bayesian_Model._prior._alphas) + "_epsilon_"+ str(epsilon) + "_scatter.png")
+	# plt.show()
+
+	plt.plot(steps[-100:], probabilities_exp_by_steps[-100:], 'r', label=('Our ExpMech'))
+	# plt.plot(T, approximate_bounds, 'g^', label=('Expmech_SS zApproximate Bound'))
+	plt.plot(steps[-100:], probabilities_lap_1_by_steps[-100:], 'b', label=('LapMech (sensitivity = 2)'))
+
+	plt.plot(steps[-100:], probabilities_lap_2_by_steps[-100:], 'g', label=('LapMech (sensitivity = 4)'))
+
+	plt.xlabel("c / (steps from correct answer, in form of Hellinger Distance)")
+	plt.ylabel("Pr[H(BI(x),r) = c]")
+	plt.title("Discrete Probabilities")
+	plt.legend()
+	plt.grid()
+	plt.savefig("(data_"+ str(observation) + "_prior_"+ str(Bayesian_Model._prior._alphas) + "_epsilon_"+ str(epsilon) + "_line.png")
+	plt.show()
+
+
+
+def discrete_probabilities_from_file(filenames):
+
+	candidates_classfied_by_steps.append(candidates_for_classify)
+ 
+	probabilities_lap_by_steps.append(pro_i)
 
 
 	plt.plot(steps[-100:], probabilities_exp_by_steps[-100:], 'ro', label=('Exp Mech'))
@@ -501,11 +553,11 @@ if __name__ == "__main__":
 	epsilon = 1.0
 	delta = 0.00000001
 	prior = dirichlet([1,1,1])
-	observation = [20,20]
+	observation = [250,250]
 	x1 = [1,499]
 	x2 = [0,500]
 	epsilons = numpy.arange(0.1, 2, 0.1)
-	datasizes = gen_datasizes((100,600),20)#[300] #[8,12,18,24,30,36,42,44,46,48]#,50,52,54,56,58,60,62,64,66,68,70,72,74,76,78,80]
+	datasizes = gen_datasizes((300,900),200)#[300] #[8,12,18,24,30,36,42,44,46,48]#,50,52,54,56,58,60,62,64,66,68,70,72,74,76,78,80]
 	percentage = [0.3,0.3,0.4]
 	datasets = gen_datasets(percentage, datasizes)
 	priors = [dirichlet([4*i,4*i,4*i]) for i in range(5,20)]
@@ -513,15 +565,27 @@ if __name__ == "__main__":
 	# accuracy_VS_prior_mean(sample_size,epsilon,delta,priors,observations)
 	# accuracy_VS_prior(sample_size,epsilon,delta,priors,observation,mean)
 	# accuracy_VS_mean(sample_size,epsilon,delta,prior)
-	accuracy_VS_datasize(epsilon,delta,prior,datasets,datasizes)
+	# accuracy_VS_datasize(epsilon,delta,prior,datasets,datasizes)
 	# hellinger_vs_l1norm(Dir(observation))
 	# global_epsilon_study(datasizes,epsilon,delta,prior)
 	# accuracy_VS_epsilon(sample_size,epsilons,delta,prior,observation)
 
 	
 	# epsilon_study(datasize,epsilon,delta,prior,x1, x2)
+	# row_discrete_probabilities(datasize,epsilon,delta,prior,observation)
 
-	# accuracy_study_discrete(datasize,epsilon,delta,prior,observation)
+	# for i in range(len(datasizes)):
+	# 	row_discrete_probabilities(datasizes[i],epsilon,delta,prior,datasets[i])
+
+
+	prior = dirichlet([1,1,1,1])
+	datasizes = gen_datasizes((400,600),200)#[300] #[8,12,18,24,30,36,42,44,46,48]#,50,52,54,56,58,60,62,64,66,68,70,72,74,76,78,80]
+	percentage = [0.25,0.25,0.25,0.25]
+	datasets = gen_datasets(percentage, datasizes)
+
+	for i in range(len(datasizes)):
+		row_discrete_probabilities(datasizes[i],epsilon,delta,prior,datasets[i])
+
 	# # accuracy_study_exponential_mechanism_SS(sample_size,epsilon,delta,prior,observation)
 	# # accuracy_study_laplace(sample_size,epsilon,delta,prior,observation)
 
