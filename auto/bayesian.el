b9ac8a71d302e22057d88ecf5f642086e785e850
Gian Pietro <farinagianpietro@gmail.com>
2018-10-09 10:01:08 -0400

    some work

---
+++ auto/bayesian.el
@@ -0,0 +1,15 @@
+(TeX-add-style-hook
+ "bayesian"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "dwork2014algorithmic"
+    "talwar"
+    "CIT-004"
+    "nissim2007smooth"
+    "zhang2016differential"
+    "foulds2016theory"
+    "xiao2012bayesian"
+    "wang2015privacy"
+    "dimitrakakis2014robust"))
+ :bibtex)
+
