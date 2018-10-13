b7ca571ca0fd7a82fd5391c404d62d07017d82f2
Gian Pietro <farinagianpietro@gmail.com>
2018-10-13 11:57:54 -0400

    added hellinger def. and fixed typos

--- TPDP2018/poster/auto/main.el
+++ TPDP2018/poster/auto/main.el
@@ -6,12 +6,12 @@
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("beamerposter" "orientation=landscape" "size=custom" "width=101.6" "height=76.2" "scale=1.4") ("babel" "english")))
    (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -30,14 +30,18 @@
    (TeX-add-symbols
     '("stmod" 1)
     '("interp" 2)
+    "diff"
     "leftfoot"
-    "rightfoot")
+    "rightfoot"
+    "DIfF"
+    "gobblespace"
+    "opspace"
+    "DiffSpace")
    (LaTeX-add-labels
     "fig_sensitivity"
     "subfig_concrete_prob_2d"
     "subfig_concrete_prob_3d"
     "subfig_prior"
-    "subfig_sampling_2d"
     "subfig_sampling_3d"
     "subfig_sampling_4d"
     "fig_sampling")
